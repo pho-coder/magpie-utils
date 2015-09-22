77adc693051a783756814e1ee57d4bd497fdc2e4
Phoenix Zhang <pho.coderzhang@gmail.com>
2015-09-22 17:44:40 +0800

    fix

---
+++ test/com/jd/bdp/magpie/utils_test.clj
@@ -0,0 +1,7 @@
+(ns com.jd.bdp.magpie.utils-test
+  (:require [clojure.test :refer :all]
+            [com.jd.bdp.magpie.utils :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
