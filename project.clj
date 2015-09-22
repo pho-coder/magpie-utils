61dfd68a6c584c9f61f7cd0ba212487112cb47c5
Phoenix Zhang <pho.coderzhang@gmail.com>
2015-09-22 17:44:09 +0800

    fix

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject magpie-utils "0.1.0.201509211355-SNAPSHOT"
+(defproject com.jd.bdp.magpie/magpie-utils "0.1.0.2015092214-SNAPSHOT"
   :description "magpie utils"
   :url "https://github.com/pho-coder/magpie-utils"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.yaml/snakeyaml "1.16"]])
