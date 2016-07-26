909ff0dba8b53b654e5040f764a59bc1f98bbc3a
Phoenix Zhang <pho.coderzhang@gmail.com>
2016-07-26 17:56:13 +0800

    update clojure version1.8.0

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject com.jd.bdp.magpie/magpie-utils "0.1.3-SNAPSHOT"
+(defproject com.jd.bdp.magpie/magpie-utils "0.1.4-SNAPSHOT"
   :description "magpie utils"
   :url "https://github.com/pho-coder/magpie-utils"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.yaml/snakeyaml "1.16"]
                  [org.clojure/data.json "0.2.6"]])
